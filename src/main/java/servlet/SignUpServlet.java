0f902ed6b37b7255898b43ab351ddaaa62a2832e
s20213108 <s20213108@HCS54M178.stdlan.hcs.ac.jp>
2023-10-25 10:28:48 +0900

    DAOテスト１

---
+++ src/main/java/servlet/SignUpServlet.java
@@ -0,0 +1,58 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SignUpServlet
+ */
+@WebServlet("/SignUpServlet")
+public class SignUpServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SignUpServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// signup.jspに遷移
+		request.getRequestDispatcher("/WEB-INF/jsp/signup.jsp").forward(request, response);
+		return;
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	/*protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 値を受け取る
+		String username = request.getParameter("userName");
+		String mailaddress = request.getParameter("mailAddress");
+		String password = request.getParameter("password");
+		
+		//DAOを使用
+		try {
+			UsersDAO usersDao = new UsersDAO();
+			usersDao.insert(username,mailaddress,password);
+		} catch (SwackException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		
+	}*/
+
+}
