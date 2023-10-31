369d9654903a1e51c9c4b233bbf40a76bb015e5c
s20213108 <s20213108@HCS54M178.stdlan.hcs.ac.jp>
2023-10-31 11:01:58 +0900

    メンバー招待（仮）

--- src/main/java/servlet/JoinMemberServlet.java
+++ src/main/java/servlet/JoinMemberServlet.java
@@ -1,12 +1,19 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.OtherUsers;
+import bean.User;
+import exception.SwackException;
+import model.UserModel;
 
 /**
  * Servlet implementation class JoinMemberServlet
@@ -28,9 +35,23 @@ public class JoinMemberServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		request.getRequestDispatcher("/WEB-INF/jsp/joinmember.jsp").forward(request, response);
-		return;
+		//セッション取得
+		HttpSession session = request.getSession();
+		User user = (User) session.getAttribute("user");
+
+		//  招待することができる人の一覧を取得
+		UserModel userModel = new UserModel();
+		try {
+			List<OtherUsers> userList = userModel.getUserList(user.getUserId());
+			request.setAttribute("userList", userList);
+			request.getRequestDispatcher("/WEB-INF/jsp/joinmember.jsp").forward(request, response);
+			return;
+		} catch (SwackException e) {
+			//  自動生成された catch ブロック
+			e.printStackTrace();
+			return;
+		}
+
 	}
 
 	/**
