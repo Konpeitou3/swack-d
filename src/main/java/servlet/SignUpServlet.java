60b312664743afc9d07296fcd9a3b45bab21a1f4
s20213108 <s20213108@HCS54M178.stdlan.hcs.ac.jp>
2023-11-20 10:23:35 +0900

    SignUpServletリファクタリング

--- src/main/java/servlet/SignUpServlet.java
+++ src/main/java/servlet/SignUpServlet.java
@@ -43,15 +43,18 @@ public class SignUpServlet extends HttpServlet {
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * userName  ユーザー名
+	 * mailAddress  メールアドレス
+	 * password  パスワード
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// 値を受け取る
-		String username = request.getParameter("userName");
-		System.out.println(username);
+		String userName = request.getParameter("userName");
+		System.out.println(userName);
 
-		String mailaddress = request.getParameter("mailAddress");
-		System.out.println(mailaddress);
+		String mailAddress = request.getParameter("mailAddress");
+		System.out.println(mailAddress);
 
 		String password = request.getParameter("password");
 		System.out.println(password);
@@ -59,19 +62,20 @@ public class SignUpServlet extends HttpServlet {
 		//入力値判定
 		StringBuilder errorMsg = new StringBuilder();
 
-		if (username == null || username.length() == 0) {
+		if (userName == null || userName.length() == 0) {
 			errorMsg.append("ユーザ名が入力されていません<br>");
 		}
-		if (mailaddress == null || mailaddress.length() == 0) {
+		if (mailAddress == null || mailAddress.length() == 0) {
 			errorMsg.append("メールアドレスが入力されていません<br>");
 		}
+		//パスワードチェック
 		if (password == null || password.length() == 0) {
 			errorMsg.append("パスワードが入力されていません<br>");
 		}
-
 		if (errorMsg.length() > 0) {
 			// エラー
 			request.setAttribute("errorMsg", errorMsg.toString());
+			//signup.jspにリダイレクト
 			request.getRequestDispatcher("/WEB-INF/jsp/signup.jsp").forward(request, response);
 			return;
 		}
@@ -81,20 +85,25 @@ public class SignUpServlet extends HttpServlet {
 			List<User> mailaddressList = usersDao.getMailAddressList();
 			for (User user : mailaddressList) {
 				System.out.println(user.getMailAddress());
-				if ((mailaddress.equals(user.getMailAddress())) == true) {
-					//if ((mailaddress.equals("negimayo@swack.com")) == true) {
+				if ((mailAddress.equals(user.getMailAddress())) == true) {
+					//入力されたメールアドレスがすでに登録済みの場合
 					System.out.println("エラー");
 					request.setAttribute("errorMsg", ERR_USERS_ISREGISTERED);
+					//signup.jspにリダイレクト
 					request.getRequestDispatcher("/WEB-INF/jsp/signup.jsp").forward(request, response);
 					return;
 				}
 			}
-			int result = usersDao.insert(username, mailaddress, password);
+			int result = usersDao.insert(userName, mailAddress, password);
 			if (result == 1) {
+				//正常にアカウントをDBに登録できたら
+				//login.jspに遷移
 				request.setAttribute("successMsg", NEW_USERS_CREATE_SUCCSESS);
 				request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
 				return;
 			} else {
+				//アカウントを登録できなかったら
+				//signup.jspにリダイレクト
 				request.setAttribute("errorMsg", NEW_USERS_CREATE_SUCCSESS);
 				request.getRequestDispatcher("/WEB-INF/jsp/signup.jsp").forward(request, response);
 			}
@@ -103,6 +112,7 @@ public class SignUpServlet extends HttpServlet {
 			// 自動生成された catch ブロック
 			e1.printStackTrace();
 			errorMsg.append("処理が正常に動きませんでした");
+			//signup.jspにリダイレクト
 			request.getRequestDispatcher("/WEB-INF/jsp/signup.jsp").forward(request, response);
 			return;
 		}
